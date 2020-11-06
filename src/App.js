396892c30a03a016f67429834c96894c3b1c2133
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-07 00:11:20 +0100

    Created DepartmentList component Linked app successfully to API with axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import DepartmentList from './components/DepartmentList'
+
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function App() {
+  const getFileList = () => {
+    let params = {
+      filter_2_above_50: "TRUE"
+    };
+    axios({
+        url: 'https://hr.dotsforthings.com/api/get_department_list.php',
+        method: 'POST',
+        data: params
+      }).then((response) => {console.log(response.data);
+      });
+
+  };
+  
+  useEffect(() => {
+    getFileList();
+  }, );
+
   return (
     <div className="App">
       <header className="App-header">
+        <DepartmentList />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +38,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HR
         </a>
       </header>
     </div>
---
+++ src/components/DepartmentList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DepartmentList = () => {
+    return (
+        <div><h2>List from component</h2></div>
+    );
+};
+
+export default DepartmentList;
