fc9d56f58ea1ce8b393fbaf830f58147bb91b890
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-07 11:44:36 +0100

    Reactstrap is  installed and working

--- src/components/DepartmentList.js
+++ src/components/DepartmentList.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Button, Card} from 'react-bootstrap'
 
 const DepartmentList = () => {
     return (
-        <div><h2>List from component</h2></div>
+        <>
+            <Card>
+                <Card.Body><h2>List from component</h2></Card.Body>
+            </Card>
+            <Button>Hola</Button>
+        </>
     );
 };
 
