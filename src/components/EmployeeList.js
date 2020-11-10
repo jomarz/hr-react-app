71849a9cd44d400c1e56f832825e58692320a040
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-10 16:24:36 +0100

    Fix thousands separator display. Fix color in doughnut chart

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,6 +6,12 @@ const EmployeeList = ({employeeList}) => {
 
     useEffect(()=> console.log(employeeList),[])
 
+    function formatNumber(num) {
+        num = parseFloat(num);
+        num = Math.floor(num);
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+      }
+
     return (
         <div className='report-list'>
         <Card className='report-header d-sm-none d-md-block'>
@@ -29,7 +35,8 @@ const EmployeeList = ({employeeList}) => {
                                 <p>{item.department_name}</p>
                             </Col>
                             <Col className='md-2 number-cell'>
-                                <p>{parseFloat(item.employee_salary).toLocaleString('es')}</p>
+                                {/* <p>{parseFloat(item.employee_salary).toLocaleString('es')}</p> */}
+                                <p>{formatNumber(item.employee_salary)}</p>
                             </Col>
                         </Row>
                     </CardBody>
